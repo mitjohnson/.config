191c0629955e9fffc618f93f774f5b1fc9a5eeca
mitchell <91237766+boring-jpg@users.noreply.github.com>
2025-03-20 23:58:37 -0400

    add easy testing

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,3 @@
 require("config.lazy")
+require("config.settings")
 
--- Tab options
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
--- Terminal
-vim.keymap.set("n", "<leader>st", function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 5)
-end)
