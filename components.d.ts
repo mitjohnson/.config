148a830be5beed4cbe4c28e867fdc920f994aea7
mitchell <91237766+boring-jpg@users.noreply.github.com>
2025-05-30 21:14:17 -0400

    chore(config): Organize git ignore, track new files.

--- components.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@vue/runtime-dom' {  // Vue <= 2.6.14
-
-  export interface GlobalComponents {
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
-  }
-
-
-}
